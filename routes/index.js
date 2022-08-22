1fc15f6f9f64a76b1e3d7e3a40c292714291e337
bevuxyna <wannhorn@gmail.com>
2022-08-22 22:38:31 +0300

    Реализовать аутентификацию и авторизацию

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const userRouter = require('./users');
+const movieRouter = require('./movies');
+const { login, createUser } = require('../controllers/users');
+const auth = require('../middlewares/auth');
+const NotFoundError = require('../errors/not-found-error');
+
+router.post('/signin', login);
+router.post('/signup', createUser);
+
+router.use(auth);
+
+router.use('/users', userRouter);
+router.use('/movies', movieRouter);
+
+router.use((req, res, next) => {
+  next(new NotFoundError('Запрашиваемый ресурс не найден'));
+});
+
+module.exports = router;
