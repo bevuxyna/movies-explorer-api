8c226a731cd70b62cb16d17b4e901aad9d39a247
bevuxyna <wannhorn@gmail.com>
2022-08-22 22:47:17 +0300

    Реализовать логирование

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const routes = require('./routes/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const errorHandler = require('./middlewares/errors');
 
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
   useNewUrlParser: true,
@@ -10,7 +13,16 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(requestLogger); // подключаем логгер запросов
+
 app.use(routes);
+
+app.use(errorLogger); // подключаем логгер ошибок
+
+app.use(errorHandler); // обработчик ошибок
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
